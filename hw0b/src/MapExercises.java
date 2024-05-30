a41b1ebaf24f8eee9e23caab4c17f559e231215b
YeHuodeShenMing <tearnforthu@gmail.com>
2024-05-30 14:52:45 +0800

    Hw0 MapExercises Finished!

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,26 +1,50 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MapExercises {
-    /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
+    /**
+     * Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> alphabet = new HashMap<>();
+        int i = 1;
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            alphabet.put(ch, i);
+            i++;
+        }
+        return alphabet;
     }
 
-    /** Returns a map from the integers in the list to their squares. For example, if the input list
-     *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
+    /**
+     * Returns a map from the integers in the list to their squares. For example, if the input list
+     * is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> square = new HashMap<>();
+        for (Integer num : nums) {
+            int product = num * num;
+            square.put(num, product);
+        }
+        return square;
     }
 
-    /** Returns a map of the counts of all words that appear in a list of words. */
+    /**
+     * Returns a map of the counts of all words that appear in a list of words.
+     */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> name = new HashMap<>();
+        for (String word : words) {
+            if (name.containsKey(word)) {
+                name.put(word, name.get(word) + 1);
+            } else {
+                name.put(word, 1);
+            }
+        }
+        return name;
     }
 }
--- hw0b/tests/MapExercisesTest.java
+++ hw0b/tests/MapExercisesTest.java
@@ -1,8 +1,4 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 import java.util.Map;
@@ -32,11 +28,19 @@ public class MapExercisesTest {
 
         Map<Integer, Integer> map = MapExercises.squares(lst);
         assertThat(map).containsExactly(
-            1, 1,
-            3, 9,
-            6, 36,
-            7, 49
+                1, 1,
+                3, 9,
+                6, 36,
+                7, 49
         );
+    }
+
+    @Test
+    public void testSquaresEmpty() {
+        List<Integer> lst = List.of();
+
+        Map<Integer, Integer> map = MapExercises.squares(lst);
+        assertThat(map).isEmpty();
     }
 
     @Test
@@ -44,20 +48,28 @@ public class MapExercisesTest {
     @DisplayName("Test countWords correctness")
     public void testCountWords() {
         List<String> lst = List.of(
-            "hug", "hug", "hug", "hug",
-            "shreyas", "shreyas", "shreyas",
-            "ergun", "ergun",
-            "cs61b"
+                "hug", "hug", "hug", "hug",
+                "shreyas", "shreyas", "shreyas",
+                "ergun", "ergun",
+                "cs61b"
         );
 
         Map<String, Integer> map = MapExercises.countWords(lst);
 
         assertThat(map).containsExactly(
-            "hug", 4,
-            "shreyas", 3,
-            "ergun", 2,
-            "cs61b", 1
+                "hug", 4,
+                "shreyas", 3,
+                "ergun", 2,
+                "cs61b", 1
         );
+    }
+
+    @Test
+    public void testCountWordsEmpty() {
+        List<String> lst = List.of();
+
+        Map<String, Integer> map = MapExercises.countWords(lst);
+        assertThat(map).isEmpty();
     }
 
 }
